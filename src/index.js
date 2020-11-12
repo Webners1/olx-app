8b3a551c1869e220abdeb2610bd7746252ed83a9
MuzammilSiddiqui1 <babarsiddiqui62@gmail.com>
2020-11-13 01:08:02 +0500

    olx App

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {HashRouter as Router} from 'react-router-dom'
+import {BrowserRouter as Router} from 'react-router-dom'
 import {createBrowserHistory} from 'history'
 import{createStore} from 'redux'
 
